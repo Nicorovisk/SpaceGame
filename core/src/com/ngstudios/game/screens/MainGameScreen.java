b26ffb75d5028cfbc215680ebed592bd4c3586da
Nicorovisk <nicolas07san@gmail.com>
2021-04-17 21:20:32 -0300

    Added Animation

--- core/src/com/ngstudios/game/screens/MainGameScreen.java
+++ core/src/com/ngstudios/game/screens/MainGameScreen.java
@@ -5,19 +5,40 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.ngstudios.game.SpaceGame;
 
 public class MainGameScreen implements Screen {
     public static final float SPEED = 120;
+    public static final float SHIP_ANIMATION_SPEED = 0.5f;
 
-    Texture img;
+    public static final int SHIP_WIDTH_PIXEL = 17;
+    public static final int SHIP_HEIGHT_PIXEL = 32;
+    public static final int SHIP_WIDTH = SHIP_WIDTH_PIXEL*3;
+    public static final int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL*3;
+
+    Animation[] rolls;
+
     float x;
     float y;
+    int roll;
+    float stateTime;
 
     SpaceGame game;
 
     public MainGameScreen(SpaceGame game){
         this.game = game;
+        y = 15;
+        x = SpaceGame.WIDTH/2 - SHIP_WIDTH /2;
+
+        roll = 2;
+        rolls = new Animation[5];
+
+        TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), SHIP_WIDTH_PIXEL,
+                SHIP_HEIGHT_PIXEL);
+
+        rolls[roll] = new Animation(SHIP_ANIMATION_SPEED,rollSpriteSheet[roll]);
     }
 
     public void show() {
@@ -40,11 +61,13 @@ public class MainGameScreen implements Screen {
             x -= SPEED * Gdx.graphics.getDeltaTime();
         }
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        stateTime += delta;
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-        game.batch.draw(img, x, y);
+        game.batch.draw((TextureRegion) rolls[roll].getKeyFrame(stateTime, true), x, y, SHIP_WIDTH, SHIP_HEIGHT);
         game.batch.end();
     }
 
